a2407edb1062d5f3dc687384d08602e6f14255e1
j-jwolf <jjwolf@noctrl.edu>
2021-07-07 14:37:24 -0500

    fixing typo in apod.js

--- js/apod.js
+++ js/apod.js
@@ -7,7 +7,7 @@ function getAPOD()
 	xhr.onload = function()
 	{
 		let responseObject = xhr.response;
-		document.getElementById("apod").innerHTML = responseObject.explanation+"<br><br>"+(responseObject.media_type === "video" ? '<iframe width="560" height="315" src='+responseObject.url+' frameborder="0" allow="acclerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' : "<img src="+(responseObject.hdurl !== undefined ? responseObject.hdurl : responseObject.url)+' alt="nasa-apod" width="960" height="auto">');
+		document.getElementById("apod").innerHTML = responseObject.explanation+"<br><br>"+(responseObject.media_type === "video" ? '<iframe width="560" height="315" src='+responseObject.url+' frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' : "<img src="+(responseObject.hdurl !== undefined ? responseObject.hdurl : responseObject.url)+' alt="nasa-apod" width="960" height="auto">');
 		try
 		{
 			if(responseObject.copyright !== undefined) {document.getElementById("copyright").innerHTML = "Copyright: "+responseObject.copyright;}
