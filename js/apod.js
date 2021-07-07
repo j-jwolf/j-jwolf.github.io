3bdb57fbd7d89d33235733506e45f4289a18a5b8
j-jwolf <jjwolf@noctrl.edu>
2021-07-07 13:59:13 -0500

    adding apod page

---
+++ js/apod.js
@@ -0,0 +1,17 @@
+function getAPOD()
+{
+	let xhr = new XMLHttpRequest();
+	xhr.open("GET", "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
+	xhr.responseType = "json";
+	xhr.send();
+	xhr.onload = function()
+	{
+		let responseObject = xhr.response;
+		document.getElementById("apod").innerHTML = responseObject.explanation+"<br><br>"+(responseObject.media_type === "video" ? '<iframe width="560" height="315" src='+responseObject.url+' frameborder="0" allow="acclerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' : "<img src="+(responseObject.hdurl !== undefined ? responseObject.hdurl : responseObject.url)+' alt="nasa-apod" width="960" height="auto">');
+		try
+		{
+			if(responseObject.copyright !== undefined) {document.getElementById("copyright").innerHTML = "Copyright: "+responseObject.copyright;}
+		} catch(e) {console.log(e);}
+	};
+}
+getAPOD();
